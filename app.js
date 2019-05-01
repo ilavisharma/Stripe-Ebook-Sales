cec7157da45ad5961a1ef83dcf1cabc45ecb9bb1
ilavisharma <lavisharma796@gmail.com>
2019-05-01 20:43:14 +0530

    handle /charge post route for stripe

--- app.js
+++ app.js
@@ -21,6 +21,28 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Charge Route
+app.post('/charge', (req, res) => {
+  const amount = 2500;
+
+  // create the customer
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken
+    })
+    .then(customer =>
+      stripe.charges.create({
+        // charge the customer
+        amount,
+        description: 'Crush It Ebook',
+        currency: 'usd',
+        customer: customer.id
+      })
+    )
+    .then(charge => res.render('success'));
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started at port ${port}`));
